7a5959dd6608b9551a66e766c6cee28b3dc06ff6
Václav Zeman <vhaisman@gmail.com>
2005-01-05 20:35:05 +0100

    Somewhat working version of encoder.
    darcs-hash:20050105193505-a77cf-8c8c756ef67d1ae0e203e081078cac366a39623b

--- lzw.lisp
+++ lzw.lisp
@@ -56,7 +56,15 @@
 (defstruct lzw-enc-ctx
   tree
   (node-count 256 :type fixnum)
-  inserted)
+  ;inserted
+  current
+  ;last-char
+)
+
+(defun fresh-lzw-enc-ctx ()
+  (let ((ctx (make-lzw-enc-ctx)))
+    (setf (lzw-enc-ctx-tree ctx) (fresh-tree))
+    ctx))
 
 
 ;; Tries to walk from node to a next node using a letter.
@@ -65,11 +73,37 @@
 ;;   (moved-to-node nil nil): It was possible to move to a next node.
 ;;   (nil inserted-node output-code): It was not possible to move to a next 
 ;;     node.
-(defun walk-tree (node letter ctx)
-  (let ((next (next-node node letter)))
+(defun lzw-enc-letter (letter ctx)
+  (let* ((node (or (lzw-enc-ctx-current ctx) (lzw-enc-ctx-tree ctx)))
+         (next (next-node node letter)))
     (if (null next)
-        (let* ((num (lzw-enc-ctx-node-count ctx))
-               (inserted-node (insert-node node (new-node num) letter)))
-          (incf (lzw-enc-ctx-node-count ctx))
-          (values inserted-node num))
-        (values next))))
+	(block true
+	  (assert (not (eq node (lzw-enc-ctx-tree ctx))))
+	  (let* ((output (node-num node)) ;; Output
+		 (num (lzw-enc-ctx-node-count ctx)) ;; Number of the new node.
+		 (root (lzw-enc-ctx-tree ctx)) ;; Root of the tree.
+		 (new-current (next-node root letter))) ;; New current node.
+	    ;; Insert new node.
+	    (insert-node node (new-node num) letter)
+	    ;; Increment node-count.
+	    (incf (lzw-enc-ctx-node-count ctx))
+	    ;; Reset current node.
+	    (setf (lzw-enc-ctx-current ctx) new-current)
+	    ;; Result is the output symbol
+	    output))
+	(block false
+          ;; Change current node.
+          (setf (lzw-enc-ctx-current ctx) next)
+          ;; Return nil as we have nothing to output yet.
+          nil))))
+
+(defun test ()
+  (let ((ss (make-string-input-stream "abaaacaabacdbaaaaaaa"))
+        (ctx (fresh-lzw-enc-ctx)))
+    (loop 
+        (let ((ch (read-char ss)))
+          (if (not ch) (return))
+          (let* ((retval (lzw-enc-letter (- (char-code ch) (char-code #\a)) ctx)))
+            (print retval)
+            ;(new-line)
+            t)))))
